c26d7b01a6746fd6263bdad08d8d9e63128c925d
wambatcode <feyfey927@gmail.com>
2025-02-03 14:18:21 +0900

    feat: Service root 파일 생성

---
+++ internal/service/root.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"goweb1/internal/repository"
+	"sync"
+)
+
+var (
+	serviceInit     sync.Once //라우터 한번만 호출하기 위함
+	serviceInstance *Service
+)
+
+type Service struct {
+	repository *repository.Repository
+}
+
+func NewService(rep *repository.Repository) *Service {
+	serviceInit.Do(func() {
+		serviceInstance = &Service{
+			repository: rep,
+		}
+	})
+
+	return serviceInstance
+}
