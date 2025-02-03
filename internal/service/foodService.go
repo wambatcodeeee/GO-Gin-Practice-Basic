c842d752ceddb7b156acfd5e8cb8f51a08dd6cad
wambatcode <feyfey927@gmail.com>
2025-02-03 15:56:20 +0900

    feat: foodService 내 CRUD 로직 추가

--- internal/service/foodService.go
+++ internal/service/foodService.go
@@ -1,6 +1,9 @@
 package service
 
-import "goweb1/internal/repository"
+import (
+	"goweb1/internal/model"
+	"goweb1/internal/repository"
+)
 
 type Food struct {
 	foodRepository *repository.FoodRepository
@@ -8,4 +11,20 @@ type Food struct {
 
 func newFoodService(foodRepository *repository.FoodRepository) *Food {
 	return &Food{foodRepository: foodRepository}
+}
+
+func (f *Food) Create(newFood *model.Food) error {
+	return f.foodRepository.Create(newFood)
+}
+
+func (f *Food) Get() []*model.Food {
+	return f.foodRepository.Get()
+}
+
+func (f *Food) Update(oldFood, newFood *model.Food) error {
+	return f.foodRepository.Update(oldFood, newFood)
+}
+
+func (f *Food) Delete(newFood *model.Food) error {
+	return f.foodRepository.Delete(newFood)
 }
